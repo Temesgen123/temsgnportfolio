1016d6b1e5d16876c4a85daf4b4e1c68d00c04b0
Temesgen Birechis <birechis@gmail.com>
2025-04-30 16:22:59 +0300

    modified project section.

--- src/assets/mywork-data.js
+++ src/assets/mywork-data.js
@@ -3,14 +3,14 @@ import project2_img from './suhillportfolio.png';
 const mywork_data = [
   {
     w_no: '1',
-    w_name: 'Web design',
+    w_name: 'CAR DEALERSHIP WEBSITE',
     w_image: project1_img,
     w_dsc:
       'Designed and developed a modern responsive car dealership website to streamline the process of showcasing available vehicles and assisting customers in finding their ideal car. Integrated a system for scheduling test drives and consultations.',
   },
   {
     w_no: '2',
-    w_name: 'Web design',
+    w_name: 'E-COMMERCE APPLICATION',
     w_image: project2_img,
     w_dsc:
       'Developed a fully resposive e-commerce web application enabling users to browse products, add items to their cart and complete purchases securely. Integrated payment gateway, user authentication, and dynamic product filtering. Created admin panel for adding new product, updating product price and users account information.',
